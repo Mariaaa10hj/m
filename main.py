2b5cd19df58081fcc17fe54ee47bc13c64abd362
Mariaaa10hj <145346800+Mariaaa10hj@users.noreply.github.com>
2023-09-18 16:05:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-print("Hello, World!")
 n1 = int(input('введите начальное число1: '))
 n2 = int(input('введите конечное число1: '))
 n3 = int(input('введите начальное число2: '))
@@ -14,9 +13,9 @@ while n3 <= n4:
     n3 = n3 + 1
 for i in measure1:
     for k in measure2:
-        if i == k:
+        if i = k:
             general.append(i)
-            break
+            
 if len(general) >= 1:
     print('пересечение равно: ', general[0], '-', general[-1])
 else:
